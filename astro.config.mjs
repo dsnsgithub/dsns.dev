c71b9f98bb989a1385ad36dbe9aaef742f6804a8
dsnsgithub <48170013+dsnsgithub@users.noreply.github.com>
2024-08-01 23:39:17 -0700

    add vercel compatability

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), react(), mdx()]
+  integrations: [tailwind(), icon(), react(), mdx()],
+  output: "server",
+  adapter: vercel()
 });
