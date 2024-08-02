9d5e6cc53b68c0a3b290813e07c3e33f64eac655
dsnsgithub <48170013+dsnsgithub@users.noreply.github.com>
2024-08-01 19:20:52 -0700

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,29 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			colors: {
+				'viola': {
+					'50': '#faf5f7',
+					'100': '#f6edf1',
+					'200': '#eedce3',
+					'300': '#e2bfcd',
+					'400': '#c98aa2',
+					'500': '#bd7790',
+					'600': '#a85a72',
+					'700': '#8f475b',
+					'800': '#773d4c',
+					'900': '#653642',
+					'950': '#3b1c24',
+				}
+			},
+			fontFamily: {
+				sans: ["DM Sans Variable", ...defaultTheme.fontFamily.sans]
+			}
+		}
+	},
+	plugins: []
+};
