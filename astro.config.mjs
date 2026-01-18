4ecc8472d8c9d299b14078fe0ad5afbe0694a00b
dsnsgithub <dominic@seung.dev>
2026-01-18 01:34:47 -0800

    fix bluesky comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel/serverless";
+
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), icon(), react(), mdx()],
-	adapter: vercel()
+  integrations: [tailwind(), icon(), react(), mdx()],
+  output: "hybrid",
+  adapter: vercel()
 });
